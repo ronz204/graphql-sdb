396837247c2287a66b963e5d5d97b9a2937b91cb
roney ruiz <roneyruizrojas@gmail.com>
2026-05-17 22:47:42 -0600

    feat: create samples of drizzle and yoga

--- source/yoga.ts
+++ source/yoga.ts
@@ -1,14 +1,49 @@
 import yoga from "@elysia/graphql-yoga"
+import { Users } from "./users";
+import { drizz } from "./drizzle"
+import { eq } from "drizzle-orm";
+
+const type1 = /* GraphQL */ `
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+`;
+
 
 export const Yoga = yoga({
   typeDefs: /* GraphQL */ `
+    ${type1}
     type Query {
-      hi: String
+      hi: String!
     }
   `,
   resolvers: {
     Query: {
       hi: () => "Hello World!",
+      users: async () => {
+        const users = await drizz.select().from(Users);
+        return users.map((user) => ({
+          id: String(user.id),
+          name: user.name,
+          email: user.email,
+        }));
+      },
+      user: async (_: any, { id }: { id: string }) => {
+        const user = await drizz.select().from(Users).where(eq(Users.id, Number(id))).limit(1).get();
+        if (!user) return null;
+        return {
+          id: String(user.id),
+          name: user.name,
+          email: user.email,
+        };
+      },
     },
   },
 });
