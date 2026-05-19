e995505dcfeffc8ff955f83b2c73fd4b853ad6d7
roney ruiz <roneyruizrojas@gmail.com>
2026-05-18 21:32:58 -0600

    feat: create health and cors plugins

--- source/boot.ts
+++ source/boot.ts
@@ -1,16 +1,14 @@
 import { env } from "@env";
 import { Yoga } from "./yoga";
 import { Elysia } from "elysia";
-import { Users } from "./users";
-import { drizz } from "./drizzle";
+
+import { HealthPlugin } from "@plugins/health.plugin";
+import { OriginsPlugin } from "@plugins/origins.plugin";
 
 export const app = new Elysia({ prefix: "/api" })
-  .get("/sample", async ({ status }) => {
-    const users = await drizz.select().from(Users);
-    return status(200, users);
-  })
-  .use(Yoga)
-  .listen(env.APP_PORT);
+  .use(HealthPlugin)
+  .use(OriginsPlugin)
+  .use(Yoga).listen(env.APP_PORT);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
 console.log(`🦊 Elysia is running at ${url}`);
