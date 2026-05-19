db1c649580563d820d16b1681a6b4faa666c9e70
roney ruiz <roneyruizrojas@gmail.com>
2026-05-18 21:55:45 -0600

    chore: discard yoga components

--- source/drizzle.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "@env";
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
-
-const client = createClient({ url: env.DB_FILE_NAME });
-export const drizz = drizzle({ client });
--- source/users.ts
+++
@@ -1,8 +0,0 @@
-import { int, sqliteTable, text } from "drizzle-orm/sqlite-core"
-
-export const Users = sqliteTable("users", {
-  id: int().primaryKey({ autoIncrement: true }),
-  name: text().notNull(),
-  email: text().notNull().unique(),
-  password: text().notNull(),
-});
