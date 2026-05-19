4a584c6afbb2a0aa3e157db2a87d719308bd1d53
roney ruiz <roneyruizrojas@gmail.com>
2026-05-19 07:50:48 -0600

    chore: purge graphql plugins temporary

--- source/boot.ts
+++ source/boot.ts
@@ -1,14 +1,12 @@
 import { env } from "@env";
 import { Elysia } from "elysia";
 
-import { ApolloPlugin } from "@plugins/apollo.plugin";
 import { HealthPlugin } from "@plugins/health.plugin";
 import { OriginsPlugin } from "@plugins/origins.plugin";
 
 export const app = new Elysia({ prefix: "/api" })
   .use(HealthPlugin)
   .use(OriginsPlugin)
-  .use(ApolloPlugin)
   .listen(env.APP_PORT);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
--- source/plugins/apollo.plugin.ts
+++
@@ -1,6 +0,0 @@
-import apollo from "@elysia/apollo";
-import { UserGraphQL } from "@graphql/users.graphql";
-
-export const ApolloPlugin = apollo({
-  typeDefs: [UserGraphQL],
-});
