4559534bec2e5063e2de2d57bd808d2d118bfdb7
roney ruiz <roneyruizrojas@gmail.com>
2026-05-19 19:41:42 -0600

    chore: purge repos and features

--- source/plugins/drizzle.plugin.ts
+++ source/plugins/drizzle.plugin.ts
@@ -3,14 +3,9 @@ import { Elysia } from "elysia";
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
-import { UserRepository } from "@repos/users.repo";
-
 export const DrizzlePlugin = new Elysia({ name: "drizzle.plugin" })
   .decorate(() => {
     const client = createClient({ url: env.DB_FILE_NAME });
     const drizz = drizzle({ client });
-
-    return {
-      userRepo: new UserRepository(drizz),
-    };
+    return { db: drizz };
   });
