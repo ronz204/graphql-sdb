fdb1eeb19922ffbc5fc773a4134b0548084d7635
roney ruiz <roneyruizrojas@gmail.com>
2026-05-21 21:21:51 -0600

    feat: create quick sample with yoga

--- source/plugins/graph.plugin.ts
+++ source/plugins/graph.plugin.ts
@@ -1,9 +1,12 @@
 import yoga from "@elysia/graphql-yoga";
 
-import ProfileGql from "@sandbox/profile.graphql" with { type: "text" };
-import WalletGql from "@sandbox/wallet.graphql" with { type: "text" };
+import SampleGql from "./sample.graphql" with { type: "text" };
 
 export const GraphPlugin = yoga({
-  typeDefs: ``,
-  resolvers: {},
+  typeDefs: SampleGql,
+  resolvers: {
+    Query: {
+      ping: () => "pong",
+    },
+  },
 });
