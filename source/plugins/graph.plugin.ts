e2809e3f34bd62af7fff56a994d298f9267a17bd
roney ruiz <roneyruizrojas@gmail.com>
2026-05-21 21:13:06 -0600

    chore: adjust tspath for graphql temporary

--- source/plugins/graph.plugin.ts
+++ source/plugins/graph.plugin.ts
@@ -1,7 +1,7 @@
 import yoga from "@elysia/graphql-yoga";
 
-import ProfileGql from "@graphql/profile.graphql" with { type: "text" };
-import WalletGql from "@graphql/wallet.graphql" with { type: "text" };
+import ProfileGql from "@sandbox/profile.graphql" with { type: "text" };
+import WalletGql from "@sandbox/wallet.graphql" with { type: "text" };
 
 export const GraphPlugin = yoga({
   typeDefs: ``,
