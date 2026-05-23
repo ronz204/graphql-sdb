2fff23c2b6176a6b0058e071fa98707ba5728cd1
roney ruiz <roneyruizrojas@gmail.com>
2026-05-23 12:23:22 -0600

    feat: complete save new user graphql

--- source/features/users-slice/users.resolvers.ts
+++ source/features/users-slice/users.resolvers.ts
@@ -2,6 +2,13 @@ import type { Resolvers } from "@graphql/generated";
 
 export const UserResolvers: Resolvers = {
   Query: {
-    users: (_, __, ctx) => ctx.userProvider.getAllUsers(),
+    users: (_, __, ctx) => {
+      return ctx.userProvider.getAllUsers();
+    },
+  },
+  Mutation: {
+    saveNewUser: (_, args, ctx) => {
+      return ctx.userProvider.saveNewUser(args.input);
+    },
   },
 };
