7ce61ac6443925b8475d72b103315a47a0df205e
roney ruiz <roneyruizrojas@gmail.com>
2026-05-23 12:23:14 -0600

    feat: complete save new user feature

--- source/features/users-slice/users.provider.ts
+++ source/features/users-slice/users.provider.ts
@@ -1,9 +1,15 @@
+import type { User } from "@prisma/client";
 import { UserRepository } from "@repos/users.repo";
+import type { NewUserDto } from "@repos/users.repo";
 
 export class UserProvider {
   constructor(private readonly repo: UserRepository) {};
 
-  public async getAllUsers() {
+  public async getAllUsers(): Promise<User[]> {
     return await this.repo.getAllUsers();
+  };
+
+  public async saveNewUser(user: NewUserDto): Promise<User> {
+    return await this.repo.saveNewUser(user);
   };
 };
--- source/repos/users.repo.ts
+++ source/repos/users.repo.ts
@@ -1,10 +1,21 @@
 import type { User } from "@prisma/client";
 import { PrismaClient } from "@prisma/client";
 
+export type NewUserDto = {
+  name: User["name"];
+  email: User["email"];
+};
+
 export class UserRepository {
   constructor(private readonly db: PrismaClient) {};
 
   public async getAllUsers(): Promise<User[]> {
     return await this.db.user.findMany();
+  };
+
+  public async saveNewUser(user: NewUserDto): Promise<User> {
+    const random = Math.floor(1000 + Math.random() * 9000);
+    const code = `${user.name.substring(0, 3).toUpperCase()}-${random}`;
+    return await this.db.user.create({ data: { ...user, code } });
   };
 };
