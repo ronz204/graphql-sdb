77b326886be03568f3097d596f74a14517fdc23b
roney ruiz <roneyruizrojas@gmail.com>
2026-05-22 21:18:45 -0600

    chore: remove users dao abstraction

--- source/features/users-slice/users.provider.ts
+++ source/features/users-slice/users.provider.ts
@@ -1,9 +1,9 @@
-import type { IUserDao } from "./users.dao";
+import { UserDao } from "./users.dao";
 
 export class UserProvider {
-  constructor(private readonly doa: IUserDao) {};
+  constructor(private readonly dao: UserDao) {};
 
   public async getAllUsers() {
-    return await this.doa.getAllUsers();
+    return await this.dao.getAllUsers();
   };
 };
