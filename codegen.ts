e0b954ca83eb2284cf777d1418ee7c575674c487
roney ruiz <roneyruizrojas@gmail.com>
2026-05-22 22:15:11 -0600

    feat: update codegen config with context

--- codegen.ts
+++ codegen.ts
@@ -1,10 +1,13 @@
 import type { CodegenConfig  } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  schema: "source/graphql/*.graphql",
+  schema: "source/features/**/*.graphql",
   generates: {
     "generated/graphql/schema.ts": {
       plugins: ["typescript", "typescript-resolvers"],
+      config: {
+        contextType: "../../source/graphql/context#GraphQLContext",
+      },
     },
   },
 };
