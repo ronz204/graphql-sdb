c6b29a871f8e7c229b12d7a856965e5b4eefea71
roney ruiz <roneyruizrojas@gmail.com>
2026-05-23 19:53:29 -0600

    feat: complete edit user graphql

--- source/features/users-slice/users.resolvers.ts
+++ source/features/users-slice/users.resolvers.ts
@@ -10,5 +10,13 @@ export const UserResolvers: Resolvers = {
     saveNewUser: (_, args, ctx) => {
       return ctx.userProvider.saveNewUser(args.input);
     },
+    editUser: (_, args, ctx) => {
+      const { id, ...input } = args.input;
+      return ctx.userProvider.editUser(id, {
+        ...input,
+        name: input.name ?? undefined,
+        email: input.email ?? undefined,
+      });
+    },
   },
 };
